83fc57517c6141ea9012b9452d9a19c6ef88ead0
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-28 13:28:27 +0200

    added packages for analyse codes

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 **TODO: Add description**
 
+```bash
+bash> mkdir twitch_client
+bash> cd twitch_client
+bash> mix new ./ --sup --app twitch_client
+```
+
+Git log the detail an information:
+
+```bash
+bash> git log --pretty="format:%ae|%an|%s"
+```
+
 ### 28 Dec 2018 by Oleg G.Kapranov
--- mix.exs
+++ mix.exs
@@ -7,18 +7,47 @@ defmodule TwitchClient.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
+      deps: deps(),
+      docs: docs()
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: applications(Mix.env),
       mod: {TwitchClient.Application, []}
     ]
   end
 
   defp deps do
-    []
+    [
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10.3", only: :test},
+      {:ex_doc, "~> 0.19.2", only: :dev},
+      {:ex_unit_notifier, "~> 0.1.4", only: :test},
+      {:mix_test_watch, "~> 0.9.0", only: :dev, runtime: false},
+      {:remix, "~> 0.0.2", only: :dev}
+    ]
+  end
+
+  defp applications(:dev), do: applications(:all) ++ [:remix]
+  defp applications(_all), do: [:logger]
+
+  defp docs do
+    [
+      name: "TwitchClient",
+      main: "TwitchClient",
+      homepage_url: "http://localhost",
+      source_url: "https://github.com/kapranov/twitch_client",
+      extras: ["README.md"]
+    ]
   end
 end
