d0bc31c53c795899276f67557f167d8966441c21
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-28 20:38:20 +0200

    added GenServer to TwitchClient.Manager and test

--- lib/twitch_client/manager.ex
+++ lib/twitch_client/manager.ex
@@ -1,3 +1,20 @@
 defmodule TwitchClient.Manager do
   @moduledoc false
+
+  use GenServer
+
+  @name __MODULE__
+  @manager :twitch_client_manager
+
+  def start_link(_opts), do: GenServer.start_link(@name, [], name: @manager)
+
+  def init(_args), do: {:ok, nil}
+
+  def token do
+    GenServer.call(@manager, :token)
+  end
+
+  def handle_call(:token, _from, state) do
+    {:reply, {:ok, state}, state}
+  end
 end
--- test/twitch_client/manager_test.exs
+++ test/twitch_client/manager_test.exs
@@ -1,3 +1,17 @@
 defmodule TwitchClient.ManagerTest do
   use ExUnit.Case, async: true
+
+  alias TwitchClient.Manager
+
+  setup do
+    start_supervised Manager
+
+    %{}
+  end
+
+  test "should return the token" do
+    {:ok, token} = Manager.token()
+
+    assert token == nil
+  end
 end
