abad6974f61e600ac9fc392a9a3b1990d9393b2b
Oleg G.Kapranov <lugatex@yahoo.com>
2018-12-29 20:30:02 +0200

    updated TwitchClient.Token

--- lib/twitch_client/token.ex
+++ lib/twitch_client/token.ex
@@ -1,3 +1,16 @@
 defmodule TwitchClient.Token do
   @moduledoc false
+
+  use Tesla
+
+  @twitch_client_id System.get_env("TWITCH_CLIENT_ID")
+  @twitch_secret System.get_env("TWITCH_SECRET")
+
+  plug Tesla.Middleware.BaseUrl, "https://id.twitch.tv/oauth2"
+  plug Tesla.Middleware.JSON
+
+  def create do
+    %Tesla.Env{body: %{"access_token" => token, "expires_in" => expiration_in_seconds}} = post("token?client_id=" <> @twitch_client_id <> "&client_secret=" <> @twitch_secret <> "&grant_type=client_credentials", "")
+    %{token: token, expires_in: expiration_in_seconds * 1000}
+  end
 end
