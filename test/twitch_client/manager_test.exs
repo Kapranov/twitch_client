f2c51a16f172dc4bfa61f6f99c0375330657153f
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-03 20:20:49 +0200

    mock - TwitchClient.ManagerTest

--- test/twitch_client/manager_test.exs
+++ test/twitch_client/manager_test.exs
@@ -1,6 +1,9 @@
 defmodule TwitchClient.ManagerTest do
   use ExUnit.Case, async: true
 
+  # import Mock
+
+  # alias TwitchClient.Token
   alias TwitchClient.Manager
 
   setup do
@@ -14,4 +17,19 @@ defmodule TwitchClient.ManagerTest do
 
     assert String.length(token) == 30
   end
+
+  test "should renew an expired token" do
+    # with_mock Token, [create: &random_token/0] do
+    #   {:ok, new_token} = Manager.token()
+    #   Process.sleep(2000)
+
+    #   {:ok, renew_token} = Manager.token()
+
+    #   assert renew_token != new_token
+    # end
+  end
+
+  # defp random_token do
+  #   %{token: UUID.uuid1(:hex), expires_in: 1000}
+  # end
 end
