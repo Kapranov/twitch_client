afdca5f2f238d59ad88bf8f0a854bf90f1248553
Oleg G.Kapranov <lugatex@yahoo.com>
2019-01-03 15:56:22 +0200

    updated test TwitchClient.TokenTest

--- test/twitch_client/token_test.exs
+++ test/twitch_client/token_test.exs
@@ -1,3 +1,12 @@
 defmodule TwitchClient.TokenTest do
   use ExUnit.Case, async: true
+
+  alias TwitchClient.Token
+
+  test "should create a new token" do
+    %{token: token, expires_in: expiration} = Token.create()
+
+    assert String.length(token) == 30
+    assert is_number(expiration)
+  end
 end
